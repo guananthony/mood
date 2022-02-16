f591c1189b910ce191076b57ace10f1da8e42fc9
Anthony Guan <guananthony@gmail.com>
2022-02-15 18:34:15 -0800

    Finish webpack config, get initial load going

---
+++ client/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import { Button } from '@mui/material';
+
+function App() {
+	return (
+		<React.Fragment>
+			<Header></Header>
+		</React.Fragment>
+	);
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,45 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('webpack-html-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode:
+	mode: process.env.NODE_ENV,
+	entry: {
+		src: './client/index.js',
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/env', '@babel/react'],
+				},
+			},
+			{
+				test: /\.s?css/,
+				exclude: /node_modules/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development',
+			template: 'index.html',
+		}),
+	],
+	devServer: {
+		static: {
+			publicPath: '/dist',
+			directory: path.resolve(__dirname, '/dist'),
+		},
+		proxy: {
+			'/api': 'http://localhost:3000',
+		},
+	},
 };
